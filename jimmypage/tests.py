c42572a2c3895c644c018b5db114ab0452390cf0
Eric Davis <ed@npri.org>
2011-09-02 14:51:29 -0700

    Test authenticated and anonymous users

--- jimmypage/tests.py
+++ jimmypage/tests.py
@@ -5,10 +5,13 @@ from django.contrib.auth.models import User, AnonymousUser
 from django.contrib import messages
 from django.db import models
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.contrib.auth import login
 
 from jimmypage.cache import request_is_cacheable, response_is_cacheable, get_cache_key
 
 class CacheabilityTest(TestCase):
+    urls = 'jimmypage.test_urls'
+
     def setUp(self):
         self.factory = RequestFactory()
 
@@ -19,13 +22,20 @@ class CacheabilityTest(TestCase):
         request = self.factory.post("/")
         self.assertFalse(request_is_cacheable(request))
 
+    def test_authenticated_and_anonymous_users(self):
+        request = self.factory.get("/")
+        request.user = AnonymousUser()
+        self.assertTrue(request_is_cacheable(request))
+
+        john = User.objects.create_user("john", "john@example.com", "secret")
+        self.client.login(username="john", password="secret")
+        response = self.client.get("/")
+        self.assertTrue(request_is_cacheable(request))
+
     def test_cacheable(self):
         req = HttpRequest()
         req.path = "/some/path"
         req.method = "GET"
-        self.assertTrue(request_is_cacheable(req))
-
-        req.user = AnonymousUser()
         self.assertTrue(request_is_cacheable(req))
 
         # TODO: ensure that messages works
